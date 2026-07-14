05054b0cf6264f6560e90a7758bedc8fc6e82ba3
Lars Hurst <lars.hurst.lh1@gmail.com>
2026-07-14 20:24:10 +0200

    feat(simulation): add versioned clock and scheduler diagnostics

--- Source/GenesisSimulation/Public/GenesisSimulationTypes.h
+++ Source/GenesisSimulation/Public/GenesisSimulationTypes.h
@@ -23,6 +23,85 @@ enum class EGenesisSimulationPhase : uint8
 };
 
 USTRUCT(BlueprintType)
+struct GENESISSIMULATION_API FGenesisSimulationClockState
+{
+    GENERATED_BODY()
+
+    static constexpr int32 CurrentSchemaVersion = 1;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Genesis|Simulation")
+    int32 SchemaVersion = CurrentSchemaVersion;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Genesis|Simulation")
+    int64 CurrentTick = 0;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Genesis|Simulation")
+    int64 TickDurationMicroseconds = 100000;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Genesis|Simulation")
+    int64 PendingSimulationMicroseconds = 0;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Genesis|Simulation")
+    EGenesisSimulationSpeed Speed = EGenesisSimulationSpeed::Normal;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Genesis|Simulation")
+    EGenesisSimulationSpeed ResumeSpeed = EGenesisSimulationSpeed::Normal;
+};
+
+USTRUCT(BlueprintType)
+struct GENESISSIMULATION_API FGenesisScheduledSystemConfig
+{
+    GENERATED_BODY()
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Genesis|Simulation")
+    FName SystemId;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Genesis|Simulation")
+    EGenesisSimulationPhase Phase = EGenesisSimulationPhase::Simulation;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Genesis|Simulation")
+    int32 Priority = 0;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Genesis|Simulation")
+    double BudgetMilliseconds = 1.0;
+};
+
+USTRUCT(BlueprintType)
+struct GENESISSIMULATION_API FGenesisSchedulerState
+{
+    GENERATED_BODY()
+
+    static constexpr int32 CurrentSchemaVersion = 1;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Genesis|Simulation")
+    int32 SchemaVersion = CurrentSchemaVersion;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Genesis|Simulation")
+    TArray<FGenesisScheduledSystemConfig> Systems;
+};
+
+USTRUCT(BlueprintType)
+struct GENESISSIMULATION_API FGenesisSystemTickMetrics
+{
+    GENERATED_BODY()
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Genesis|Simulation")
+    FName SystemId;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Genesis|Simulation")
+    double DurationMilliseconds = 0.0;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Genesis|Simulation")
+    double BudgetMilliseconds = 0.0;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Genesis|Simulation")
+    bool bBudgetExceeded = false;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Genesis|Simulation")
+    int32 TotalBudgetOverruns = 0;
+};
+
+USTRUCT(BlueprintType)
 struct GENESISSIMULATION_API FGenesisTickMetrics
 {
     GENERATED_BODY()
@@ -35,4 +114,7 @@ struct GENESISSIMULATION_API FGenesisTickMetrics
 
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Genesis|Simulation")
     int32 BudgetOverrunCount = 0;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Genesis|Simulation")
+    TArray<FGenesisSystemTickMetrics> Systems;
 };
