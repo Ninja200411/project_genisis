676e3e8d5f2cb2eb2d1d352239d6e65509a22291
Lars Hurst <lars.hurst.lh1@gmail.com>
2026-07-14 19:25:10 +0200

    build: add GenesisCore module

---
+++ Source/GenesisCore/GenesisCore.Build.cs
@@ -0,0 +1,10 @@
+using UnrealBuildTool;
+
+public class GenesisCore : ModuleRules
+{
+    public GenesisCore(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+        PublicDependencyModuleNames.AddRange(new[] { "Core", "CoreUObject" });
+    }
+}
