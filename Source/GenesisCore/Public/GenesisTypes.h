7c9e8d324572bd8441007ec633d49f9f2c1be894
Lars Hurst <lars.hurst.lh1@gmail.com>
2026-07-14 19:52:33 +0200

    fix(core): use Blueprint-compatible entity id type

--- Source/GenesisCore/Public/GenesisTypes.h
+++ Source/GenesisCore/Public/GenesisTypes.h
@@ -8,14 +8,19 @@ struct GENESISCORE_API FGenesisEntityId
 {
     GENERATED_BODY()
 
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
-    uint64 Value = 0;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Genesis|Entity")
+    int64 Value = 0;
 
-    bool IsValid() const { return Value != 0; }
+    bool IsValid() const { return Value > 0; }
 
     friend bool operator==(const FGenesisEntityId& Left, const FGenesisEntityId& Right)
     {
         return Left.Value == Right.Value;
+    }
+
+    friend bool operator!=(const FGenesisEntityId& Left, const FGenesisEntityId& Right)
+    {
+        return !(Left == Right);
     }
 };
 
