1b5bc2000e85667cb3ab9fe4e1900e1776364e03
Lars Hurst <lars.hurst.lh1@gmail.com>
2026-07-14 19:49:39 +0200

    fix(build): align game target with Unreal 5.8

--- Source/ProjectGenesis.Target.cs
+++ Source/ProjectGenesis.Target.cs
@@ -6,8 +6,8 @@ public class ProjectGenesisTarget : TargetRules
     public ProjectGenesisTarget(TargetInfo Target) : base(Target)
     {
         Type = TargetType.Game;
-        DefaultBuildSettings = BuildSettingsVersion.V5;
-        IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+        DefaultBuildSettings = BuildSettingsVersion.V7;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_8;
         ExtraModuleNames.AddRange(new[] { "ProjectGenesis" });
     }
 }
