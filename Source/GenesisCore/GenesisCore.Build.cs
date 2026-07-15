15d652d30bfd67dc00aebc266103cf092324156c
Lars Hurst <lars.hurst.lh1@gmail.com>
2026-07-15 17:18:39 +0200

    build(core): add JSON serialization dependency

--- Source/GenesisCore/GenesisCore.Build.cs
+++ Source/GenesisCore/GenesisCore.Build.cs
@@ -5,6 +5,11 @@ public class GenesisCore : ModuleRules
     public GenesisCore(ReadOnlyTargetRules Target) : base(Target)
     {
         PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-        PublicDependencyModuleNames.AddRange(new[] { "Core", "CoreUObject" });
+        PublicDependencyModuleNames.AddRange(new[]
+        {
+            "Core",
+            "CoreUObject",
+            "Json"
+        });
     }
 }
